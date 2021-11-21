06f12fedd9d9b746ee1e8863bb8ca4e15f366d8c
sgsellan <sgsellan@gmail.com>
2021-11-21 11:49:24 -0500

    init includes

--- __init__.py
+++ __init__.py
@@ -1,1 +1,5 @@
 from . edge_indeces import edge_indeces
+from . regular_square_mesh import regular_square_mesh
+from . linear_elasticity_stiffness import linear_elasticity_stiffness
+from . linear_elasticity import linear_elasticity
+from . signed_distance_polygon import signed_distance_polygon
