2bf8f0f1b7d2ea2f8f7cde11e0395d15092f5724
sgsellan <sgsellan@gmail.com>
2021-11-22 09:50:57 -0500

    fd partial derivative non uniform grid

--- fd_partial_derivative.py
+++ fd_partial_derivative.py
@@ -3,7 +3,7 @@ from numpy.core.function_base import linspace
 from scipy.sparse import csr_matrix
 
 
-def fd_partial_derivative(gs=10,h=(1/9.0),direction=0):
+def fd_partial_derivative(gs=np.array([10,10]),h=np.array([1/9.0,1/9.0]),direction=0):
     # Given a regular finite-difference grid described by the number of nodes 
     # on each side, the grid spacing and a desired direction, construct a sparse matrix 
     # to compute first partial derivatives in the given direction onto the 
@@ -13,8 +13,8 @@ def fd_partial_derivative(gs=10,h=(1/9.0),direction=0):
     #
     # Input:
     #       Optional:
-    #               gs int grid size
-    #               h float spacing between nearest grid nodes
+    #               gs #dim int numpy array of grid sizes [nx,ny]
+    #               h #dim float numpy array of spacing between nearest grid nodes [hx,hy]
     #               direction integer index of direction (x is 0, y is 1)
     #
     # Output:
@@ -26,39 +26,42 @@ def fd_partial_derivative(gs=10,h=(1/9.0),direction=0):
         # First, get the indeces of all possible bottom-left corners
         # which are 1:(gs-2) + gs*(1:(gs-2))
         # One of these is a kron prod, the other is a repetition
-        horizontal_indeces = linspace(0,gs-2,gs-1,dtype=int)
-        vertical_indeces = linspace(0,gs-1,gs,dtype=int)
+        horizontal_indeces = linspace(0,gs[0]-2,gs[0]-1,dtype=int)
+        vertical_indeces = linspace(0,gs[1]-1,gs[1],dtype=int)
         #print(horizontal_indeces) 
         #print(vertical_indeces) 
-        horizontal_indeces_rep = np.tile(horizontal_indeces,gs)
-        vertical_indeces_rep = np.kron(vertical_indeces,np.ones((gs-1),dtype=int))
+        horizontal_indeces_rep = np.tile(horizontal_indeces,gs[1])
+        vertical_indeces_rep = np.kron(vertical_indeces,np.ones((gs[0]-1),dtype=int))
         #print(horizontal_indeces_rep) 
         #print(vertical_indeces_rep) 
-        vectorized_indeces = horizontal_indeces_rep + gs*vertical_indeces_rep
+        vectorized_indeces = horizontal_indeces_rep + gs[0]*vertical_indeces_rep
         #print(vectorized_indeces)
         J = np.concatenate((vectorized_indeces,vectorized_indeces+1))
-        I = np.concatenate((linspace(0,(gs-1)*gs - 1,(gs-1)*gs,dtype=int),linspace(0,(gs-1)*gs - 1,(gs-1)*gs,dtype=int)))
-        vals = np.concatenate(( -np.ones((gs*(gs-1))), np.ones((gs*(gs-1)))  ))
+        I = np.concatenate((linspace(0,(gs[0]-1)*gs[1] - 1,(gs[0]-1)*gs[1],dtype=int),linspace(0,(gs[0]-1)*gs[1] - 1,(gs[0]-1)*gs[1],dtype=int)))
+        vals = np.concatenate(( -np.ones(((gs[0]-1)*gs[1])), np.ones(((gs[0]-1)*gs[1]))  ))
         # Build scipy matrix
-        D = csr_matrix((vals,(I,J)),shape=(gs*(gs-1),gs*gs))/h
+        #print(I)
+        #print(J)
+        #print(vals)
+        D = csr_matrix((vals,(I,J)),shape=(gs[1]*(gs[0]-1),gs[0]*gs[1]))/h[0]
     elif direction==1:
         # First, get the indeces of all possible bottom-left corners
         # which are 1:(gs-2) + gs*(1:(gs-2))
         # One of these is a kron prod, the other is a repetition
-        vertical_indeces = linspace(0,gs-2,gs-1,dtype=int)
-        horizontal_indeces = linspace(0,gs-1,gs,dtype=int)
+        vertical_indeces = linspace(0,gs[1]-2,gs[1]-1,dtype=int)
+        horizontal_indeces = linspace(0,gs[0]-1,gs[0],dtype=int)
         #print(horizontal_indeces) 
         #print(vertical_indeces) 
-        horizontal_indeces_rep = np.tile(horizontal_indeces,gs-1)
-        vertical_indeces_rep = np.kron(vertical_indeces,np.ones((gs),dtype=int))
+        horizontal_indeces_rep = np.tile(horizontal_indeces,gs[1]-1)
+        vertical_indeces_rep = np.kron(vertical_indeces,np.ones((gs[0]),dtype=int))
         #print(horizontal_indeces_rep) 
         #print(vertical_indeces_rep) 
-        vectorized_indeces = horizontal_indeces_rep + gs*vertical_indeces_rep
+        vectorized_indeces = horizontal_indeces_rep + gs[0]*vertical_indeces_rep
         #print(vectorized_indeces)
-        J = np.concatenate((vectorized_indeces,vectorized_indeces+gs))
-        I = np.concatenate((linspace(0,(gs-1)*gs - 1,(gs-1)*gs,dtype=int),linspace(0,(gs-1)*gs - 1,(gs-1)*gs,dtype=int)))
-        vals = np.concatenate(( -np.ones((gs*(gs-1))), np.ones((gs*(gs-1)))  ))
+        J = np.concatenate((vectorized_indeces,vectorized_indeces+gs[0]))
+        I = np.concatenate((linspace(0,(gs[1]-1)*gs[0] - 1,(gs[1]-1)*gs[0],dtype=int),linspace(0,(gs[1]-1)*gs[0] - 1,(gs[1]-1)*gs[0],dtype=int)))
+        vals = np.concatenate(( -np.ones((gs[0]*(gs[1]-1))), np.ones((gs[0]*(gs[1]-1)))  ))
         # Build scipy matrix
-        D = csr_matrix((vals,(I,J)),shape=(gs*(gs-1),gs*gs))/h   
+        D = csr_matrix((vals,(I,J)),shape=(gs[0]*(gs[1]-1),gs[0]*gs[1]))/h[1]   
 
     return D
--- unit_tests/fd_partial_derivative_unit_test.py
+++ unit_tests/fd_partial_derivative_unit_test.py
@@ -1,6 +1,4 @@
 import numpy as np
-from scipy.stats import multivariate_normal
-import matplotlib.pyplot as plt
 import sys
 
 sys.path.append("..")
@@ -8,22 +6,23 @@ from fd_partial_derivative import fd_partial_derivative
 
 
 # Choose grid size
-gs = 100
+gs = np.array([19,15])
+h = 1.0/(gs-1)
 
 # Build a grid
-x, y = np.meshgrid(np.linspace(0,1,gs),np.linspace(0,1,gs))
+x, y = np.meshgrid(np.linspace(0,1,gs[0]),np.linspace(0,1,gs[1]))
 V = np.concatenate((np.reshape(x,(-1, 1)),np.reshape(y,(-1, 1))),axis=1)
 
 # Build staggered grid in x direction
-x, y = np.meshgrid(np.linspace(0,1,gs-1),np.linspace(0,1,gs))
+x, y = np.meshgrid(np.linspace(0,1,gs[0]-1),np.linspace(0,1,gs[1]))
 Vx = np.concatenate((np.reshape(x,(-1, 1)),np.reshape(y,(-1, 1))),axis=1)
 # Build staggered grid in y direction
-x, y = np.meshgrid(np.linspace(0,1,gs),np.linspace(0,1,gs-1))
+x, y = np.meshgrid(np.linspace(0,1,gs[0]),np.linspace(0,1,gs[1]-1))
 Vy = np.concatenate((np.reshape(x,(-1, 1)),np.reshape(y,(-1, 1))),axis=1)
 
-
-Dx = fd_partial_derivative(gs=gs,h=(1.0/(gs-1)),direction=0)
-Dy = fd_partial_derivative(gs=gs,h=(1.0/(gs-1)),direction=1)
+# Build partial derivative matrices
+Dx = fd_partial_derivative(gs=gs,h=h,direction=0)
+Dy = fd_partial_derivative(gs=gs,h=h,direction=1)
 
 # all rows must sum up to zero (i.e. a constant function has zero derivative)
 assert((np.isclose(Dx.sum(axis=1),np.zeros((Dx.shape[0],1)))).all())
@@ -34,7 +33,7 @@ f = 2*V[:,0] + 5*V[:,1]
 computed_derivative_x = Dx*f
 computed_derivative_y = Dy*f
 # Derivatives must be 2.0 and 5.0, respectively
-assert((np.isclose(computed_derivative_x,2.0*np.ones((computed_derivative_y.shape[0])))).all())
+assert((np.isclose(computed_derivative_x,2.0*np.ones((computed_derivative_x.shape[0])))).all())
 assert((np.isclose(computed_derivative_y,5.0*np.ones((computed_derivative_y.shape[0])))).all())
 
 # Convergence test
@@ -42,20 +41,23 @@ linf_norm_x = 100.0
 linf_norm_y = 100.0
 print("This experiment should print a set of decreasing values, converging")
 print("towards zero and decreasing roughly by half in each iteration")
-for power in range(1,13,1):
-    gs = 2**power
+for power in range(3,13,1):
+    gs = np.array([2**power,2**power - 2])
+    h = 1.0/(gs-1)
     # Build a grid
-    x, y = np.meshgrid(np.linspace(0,1,gs),np.linspace(0,1,gs))
+    x, y = np.meshgrid(np.linspace(0,1,gs[0]),np.linspace(0,1,gs[1]))
     V = np.concatenate((np.reshape(x,(-1, 1)),np.reshape(y,(-1, 1))),axis=1)
+
     # Build staggered grid in x direction
-    x, y = np.meshgrid(np.linspace(0,1,gs-1),np.linspace(0,1,gs))
+    x, y = np.meshgrid(np.linspace(0,1,gs[0]-1),np.linspace(0,1,gs[1]))
     Vx = np.concatenate((np.reshape(x,(-1, 1)),np.reshape(y,(-1, 1))),axis=1)
     # Build staggered grid in y direction
-    x, y = np.meshgrid(np.linspace(0,1,gs),np.linspace(0,1,gs-1))
+    x, y = np.meshgrid(np.linspace(0,1,gs[0]),np.linspace(0,1,gs[1]-1))
     Vy = np.concatenate((np.reshape(x,(-1, 1)),np.reshape(y,(-1, 1))),axis=1)
-    # Build derivative matrices
-    Dx = fd_partial_derivative(gs=gs,h=(1.0/(gs-1)),direction=0)
-    Dy = fd_partial_derivative(gs=gs,h=(1.0/(gs-1)),direction=1)
+
+    # Build partial derivative matrices
+    Dx = fd_partial_derivative(gs=gs,h=h,direction=0)
+    Dy = fd_partial_derivative(gs=gs,h=h,direction=1)
     # Build non-linear function
     f = np.cos(V[:,0]) + np.sin(V[:,1])
     # Derivatives on staggered grids
