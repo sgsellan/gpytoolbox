c2acfa6d14413bda6689a494ee76279454e487e5
sgsellan <sgsellan@gmail.com>
2021-11-22 10:44:03 -0500

    note on vertex order

--- regular_square_mesh.py
+++ regular_square_mesh.py
@@ -3,13 +3,19 @@ import numpy as np
 def regular_square_mesh(gs):
     # Generates a regular mesh of a one by one square
     #
+    # Note: the ordering of the vertices is increasing by rows and then columns, 
+    #   so [0,0], [h,0], [2*h,0],...,[h,h],[2*h,h],...,[1-h,1],[1,1]
+    #
     # Input:
     #       gs int number of vertices on each side
     #
     # Output:
     #       V #V by d numpy array of mesh vertex positions
     #       F #F by d+1 int numpy array of mesh face indeces into V
-    
+    #
+    #
+    #
+    #
     x, y = np.meshgrid(np.linspace(0,1,gs),np.linspace(0,1,gs))
     v = np.concatenate((np.reshape(x,(-1, 1)),np.reshape(y,(-1, 1))),axis=1)
 
