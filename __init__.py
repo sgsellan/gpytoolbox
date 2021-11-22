6c66ff798cb0d40b9b7948fbc4801b6670779591
sgsellan <sgsellan@gmail.com>
2021-11-22 17:39:10 -0500

    added ray polyline intersect

--- __init__.py
+++ __init__.py
@@ -4,3 +4,4 @@ from . linear_elasticity_stiffness import linear_elasticity_stiffness
 from . linear_elasticity import linear_elasticity
 from . signed_distance_polygon import signed_distance_polygon
 from . metropolis_hastings import metropolis_hastings
+from . ray_polyline_intersect import ray_polyline_intersect
