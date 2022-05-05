c2dd095d887c7bb1633c1c917c6151965e2a8bb2
sgsellan <sgsellan@gmail.com>
2022-05-05 07:55:04 -0400

    bug

--- gpytoolbox/fd_grad_octree.py
+++ gpytoolbox/fd_grad_octree.py
@@ -7,4 +7,4 @@ def fd_grad_octree(V,Q):
     Dx,staggered_x =  fd_partial_derivative_octree(V,Q,direction=0)
     Dy,staggered_y =  fd_partial_derivative_octree(V,Q,direction=1)
     Dz,staggered_z =  fd_partial_derivative_octree(V,Q,direction=2)
-    return vstack((Dx,Dy,Dz)), vstack((staggered_x,staggered_y,staggered_z))
+    return vstack((Dx,Dy,Dz)), np.vstack((staggered_x,staggered_y,staggered_z))
