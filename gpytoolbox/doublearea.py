00f93331ee44e1a9921baa66849ebb5be9b8962a
Oded Stein <oded.stein@columbia.edu>
2022-07-11 13:08:21 -0400

    Fixed doublearea and doublearea test description, and split into intrinsic/extrinsic functions

--- gpytoolbox/doublearea.py
+++ gpytoolbox/doublearea.py
@@ -1,17 +1,17 @@
 import numpy as np
+from gpytoolbox.halfedge_lengths_squared import halfedge_lengths_squared
 from gpytoolbox.edge_indeces import edge_indeces
-from gpytoolbox.halfedge_lengths import halfedge_lengths
-
+from gpytoolbox.doublearea_intrinsic import doublearea_intrinsic
 
 def doublearea(V,F=None):
-    # Builds the finite elements gradient matrix using a piecewise linear hat functions basis.
+    # Construct the doublearea of each element of a line or triangle mesh
     #
     # Input:
-    #       V #V by 3 numpy array of mesh vertex positions
-    #       F #F by 3 int numpy array of face/edge vertex indices into V
+    #       V  #V by 3 numpy array of mesh vertex positions
+    #       F  #F by 3 int numpy array of face/edge vertex indices into V
     #
     # Output:
-    #       A #F vector of twice the (unsigned) area/length 
+    #       A  #F vector of twice the (unsigned) area/length 
 
     # if you didn't pass an F then this is a ordered polyline
     if (F is None):
@@ -26,15 +26,7 @@ def doublearea(V,F=None):
         dblA = 2.0*A
         
     if simplex_size==3:
-        # There are two options: dimension two or three. If it's two, we'll add a third zero dimension for convenience
-        if dim==2:
-            V = np.hstack((V,np.zeros((V.shape[0],1))))
-
-        l = halfedge_lengths(V,F)
-
-        # Using Kahan's formula
-        # https://people.eecs.berkeley.edu/~wkahan/Triangle.pdf
-        a,b,c = l[:,0], l[:,1], l[:,2]
-        dblA = 0.5 * np.sqrt((a+(b+c)) * (c-(a-b)) * (c+(a-b)) * (a+(b-c)))
+        l_sq = halfedge_lengths_squared(V,F)
+        dblA = doublearea_intrinsic(l_sq,F)
 
     return dblA
---
+++ test/test_doublearea_intrinsic.py
@@ -0,0 +1,31 @@
+from .context import gpytoolbox as gpy
+from .context import numpy as np
+from .context import unittest
+import igl
+
+class TestDoubleareaIntrinsic(unittest.TestCase):
+    def test_equilateral_triangle(self):
+        c = np.random.default_rng().random() + 0.1
+
+        l_sq = c * np.array([[1.,1.,1.]])
+        f = np.array([[0,1,2]])
+
+
+
+        A = gpy.doublearea_intrinsic(l_sq,f)
+        self.assertTrue(np.isclose(A[0], 2.*np.sqrt(3.)/4. * c))
+
+    def test_many_equilateral_triangles(self):
+        c = np.random.default_rng().random() + 0.1
+
+        _,f = gpy.regular_square_mesh(40)
+        l_sq = c * np.ones(f.shape)
+
+        A = gpy.doublearea_intrinsic(l_sq,f)
+        self.assertTrue(np.isclose(A, 2.*np.full(f.shape[0],np.sqrt(3.)/4. * c)).all())
+
+        
+
+
+if __name__ == '__main__':
+    unittest.main()
