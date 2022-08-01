698d12213baf03c1c5e0ea21e888e1c2b3fdb3e6
sgsellan <sgsellan@gmail.com>
2022-08-01 19:47:07 -0400

    removed default

--- src/gpytoolbox/fd_grad.py
+++ src/gpytoolbox/fd_grad.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy.sparse import vstack
 from .fd_partial_derivative import fd_partial_derivative
 
-def fd_grad(gs,h=np.array([1/9.0,1/9.0])):
+def fd_grad(gs,h):
     """Finite difference gradient on a grid
 
     Given a regular finite-difference grid described by the number of nodes on each side, the grid spacing and a desired direction, construct a sparse matrix to compute gradients with each component defined on its respective staggered grid
@@ -10,9 +10,9 @@ def fd_grad(gs,h=np.array([1/9.0,1/9.0])):
     Parameters
     ----------
     gs : numpy int array
-        Grid size [nx,ny]
+        Grid size [nx,ny(,nz)]
     h : numpy double array
-        Spacing between grid points [hx,hy]
+        Spacing between grid points [hx,hy(,hz)]
 
     Returns
     -------
