1c98799821b4d66069608dbda7c5390afaa4e8b6
sgsellan <sgsellan@gmail.com>
2022-08-01 20:06:34 -0400

    in element aabb docstring

--- src/gpytoolbox/in_element_aabb.py
+++ src/gpytoolbox/in_element_aabb.py
@@ -1,10 +1,41 @@
 import numpy as np
 
 def in_element_aabb(queries,V,F):
-    # TODO 
+    """Finite element gradient matrix
 
-    from gpytoolbox_bindings import _in_element_aabb_cpp_impl
+    Given a triangle mesh or a polyline, computes the finite element gradient matrix assuming piecewise linear hat function basis.
 
-    i = _in_element_aabb_cpp_impl(queries,V,F.astype(np.int32))
+    Parameters
+    ----------
+    queries : numpy double array
+        Matrix of query point coordinates
+    V : numpy double array
+        Matrix of vertex coordinates
+    F : numpy int array, optional (default None)
+        Matrix of triangle (2D) or tetrahedron (3D) indices
 
-    return i
+    Returns
+    -------
+    I : numpy int array
+        Vector of indeces into F of the elements that contain each query point (-1 means no containing element).
+
+    See Also
+    --------
+
+    Notes
+    -----
+
+    Examples
+    --------
+    TO-DO
+    """
+    try:
+        from gpytoolbox_bindings import _in_element_aabb_cpp_impl
+    except:
+        raise ImportError("Gpytoolbox cannot import its C++ decimate binding.")
+
+    
+
+    I = _in_element_aabb_cpp_impl(queries,V,F.astype(np.int32))
+
+    return I
