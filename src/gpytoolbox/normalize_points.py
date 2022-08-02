1a4f8bec2d667076c981d74de31181f6112d31e8
sgsellan <sgsellan@gmail.com>
2022-08-02 15:14:34 -0400

    normalize points

--- src/gpytoolbox/normalize_points.py
+++ src/gpytoolbox/normalize_points.py
@@ -1,18 +1,26 @@
 import numpy as np
 
 def normalize_points(v,center=None):
-    # Translate and scale and arbitrary point set so that it's contained
-    # tightly into a 1 by 1 # (by 1) cube, centered at zero by default.
-    # Simple yet useful to test code without worrying about scale-dependencies
-    #
-    # Inputs:
-    #       v #v by dim numpy array of point position coordinates
-    #       Optional:
-    #           center dim numpy array where to center mesh (zero by default)
-    #
-    # Output:
-    #       u #v by dim numpy array of output point position coordinates
-    #
+    """Make points fit into a side-length one cube
+    
+    Translate and scale and arbitrary point set so that it's contained tightly into a 1 by 1 # (by 1) cube, centered at zero by default. Simple yet useful to test code without worrying about scale-dependencies.
+
+    Parameters
+    ----------
+    v : numpy double array
+        Matrix of point position coordinates
+    center : numpy double array (optional, None by default)
+        Where to center the mesh (if None, centered at zero)
+
+    Returns
+    -------
+    u : numpy double array
+        Normalized point position coordinates
+
+    Examples
+    --------
+    TO-DO
+    """
     
     # Dimension:
     dim = v.shape[1]
