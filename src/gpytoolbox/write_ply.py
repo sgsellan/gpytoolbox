57de5e89027463c2e9c650a6235ce2ea9093a2a6
sgsellan <sgsellan@gmail.com>
2022-08-02 09:29:20 -0400

    docstring and test for write_ply

--- src/gpytoolbox/write_ply.py
+++ src/gpytoolbox/write_ply.py
@@ -2,16 +2,37 @@ import numpy as np
 from gpytoolbox.colormap import colormap
 from gpytoolbox.apply_colormap import apply_colormap
 
-def write_ply(filename,vertices,faces=None,colors=None):
-    # Writes a triangle mesh with colors into ply file format,
-    # in a way consistent with, e.g., importing to Blender 
-    # 
-    # Input:
-    #       filename a string of the full path where to write the mesh
-    #               (will be overwritten if exists)
-    #       V #V by 3 numpy array of mesh vertex positions
-    #       F #F by 3 numpy array of mesh face indeces on V
-    #
+def write_ply(filename,vertices,faces=None,colors=None,cmap='BuGn'):
+    """Store triangle mesh into .ply file format
+    
+    Writes a triangle mesh (optionally with per-vertex colors) into the ply file format, in a way consistent with, e.g., importing to Blender.
+
+    Parameters
+    ----------
+    filename : str
+        Name of the file ending in ".ply" to which to write the mesh
+    vertices : numpy double array
+        Matrix of mesh vertex coordinates
+    faces : numpy int array, optional (default None)
+        Matrix of triangle face indices into vertices. If none, only the vertices will be written (e.g., a point cloud)
+    colors : numpy double array, optional (default None)
+        Array of per-vertex colors. It can be a matrix of per-row RGB values, or a vector of scalar values that gets transformed by a colormap.
+    cmap : str, optional (default 'BuGn')
+        Name of colormap used to transform the color values if they are a vector of scalar function values (if colors is a matrix of RGB values, this parameter will not be used). Should be a valid input to `colormap`.
+
+    See Also
+    --------
+    write_mesh, colormap.
+
+    Notes
+    -----
+    This function is not optimized and covers the very specific funcionality of saving a mesh with per-vertex coloring that can be imported into Blender or other software. If you wish to write a mesh for any other purpose, we strongly recommend you use write_mesh instead.
+
+    Examples
+    --------
+    TODO
+    """
+
     vertices = vertices.astype(float)
     f = open(filename,"w")
     f.write("ply\nformat {} 1.0\n".format('ascii'))
@@ -38,7 +59,7 @@ def write_ply(filename,vertices,faces=None,colors=None):
             f.write("{} {} {}\n".format(vertices[i,0],vertices[i,1],vertices[i,2]))
     else:
         if (colors.ndim==1 or colors.shape[1]==1): # color is scalar values
-            C = apply_colormap(colormap('BuGn', 200), colors)
+            C = apply_colormap(colormap(cmap, 200), colors)
         else:
             if np.max(colors)<=1:
                 C = np.round(colors*255)
