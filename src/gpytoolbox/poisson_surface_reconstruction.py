8b8fee7e94ca93660c27b808c26713c87cfb02f9
sgsellan <sgsellan@gmail.com>
2022-12-17 15:57:14 -0500

    efficient grid variances computation

--- src/gpytoolbox/poisson_surface_reconstruction.py
+++ src/gpytoolbox/poisson_surface_reconstruction.py
@@ -7,14 +7,39 @@ from .fd_interpolate import fd_interpolate
 from .matrix_from_function import matrix_from_function
 from .compactly_supported_normal import compactly_supported_normal
 
-def poisson_surface_reconstruction(P,N,gs=None,h=None,corner=None,screened=False,sigma_n=0.0,sigma=0.1):
 
-    # To do: set gs, h, corner to something that makes sense given P if all/some are None
+# TO DO:
+# - Covariance solve on reduced laplacian eigenvalue subspace
+
+def poisson_surface_reconstruction(P,N,gs=None,h=None,corner=None,stochastic=True,sigma_n=0.0,sigma=0.1,verbose=True):
+
+    # Kernel function for the Gaussian process
     def kernel_fun(X,Y):
-        return compactly_supported_normal(X-Y,n=3,sigma=0.1)
+        return compactly_supported_normal(X-Y,n=3,sigma=np.mean(h))
 
+    # Set problem parameters to values that make sense
     dim = P.shape[1]
     assert(dim == N.shape[1])
+
+    
+    if (gs is None and h is None and corner is None):
+        # Default to a grid that is 100x100x...x100
+        gs = 100*np.ones(dim,dtype=int)
+
+    envelope_mult = 1.5 # how tightly we want to envelope the data
+    if (gs is None):
+        assert(h is not None)
+        assert(corner is not None)
+        gs = np.floor((np.max(envelope_mult*P,axis=0) - corner)/h).astype(int)
+        print(gs)
+    else:
+        assert(h is None)
+        assert(corner is None)
+        h = (np.max(envelope_mult*P,axis=0) - np.min(envelope_mult*P,axis=0))/gs
+        corner = np.min(envelope_mult*P,axis=0)
+    assert(gs.shape[0] == dim)
+
+    eps = 0.000001 # very tiny value to regularize matrix rank
 
     # Estimate sampling density at each point in P
     W_weights = fd_interpolate(P,gs=(gs+1),h=h,corner=(corner-0.5*h))
@@ -23,9 +48,13 @@ def poisson_surface_reconstruction(P,N,gs=None,h=None,corner=None,screened=False
     image_blurred_vectorized = np.reshape(image_blurred,(np.prod(gs+1),1),order='F')
     sampling_density = W_weights @ image_blurred_vectorized
         
-        
-
     # Step 1: Gaussian process interpolation from N to a regular grid
+    if verbose:
+        print("Step 1: Gaussian process interpolation from N to a regular grid")
+        # Log time to compute the kernel matrix
+        import time
+        t0 = time.time()
+
     means = []
     covs = []
     for dd in range(dim):
@@ -37,14 +66,47 @@ def poisson_surface_reconstruction(P,N,gs=None,h=None,corner=None,screened=False
         # generate grid vertices of dimension dim
         grid_vertices = np.meshgrid(*[np.linspace(corner_dd[dd], corner_dd[dd] + (gs_dd[dd]-1)*h[dd], gs_dd[dd]) for dd in range(dim)])
         grid_vertices = np.array(grid_vertices).reshape(dim, -1).T
+        
+        if verbose:
+            
+            t00 = time.time()
+        # k1 is the kernel matrix between grid vertices, which we could compute with
+        # k1 = matrix_from_function(kernel_fun, grid_vertices, grid_vertices)
+        # but that takes a long to compute if we don't leverage that we know the sparsity pattern beforehand. In fact, we know that the only non-zero entries are of vertices and their second-level neighbors.
+        order_neighbors = 2
+        neighbors = np.arange(-order_neighbors,order_neighbors+1,dtype=int)
+        for dd2 in range(dim-1):
+            neighbors_along_dim =np.arange(-order_neighbors,order_neighbors+1,dtype=int)*np.prod(gs_dd[:dd2+1])
+            previous_neighbors = np.kron(neighbors,np.ones(neighbors_along_dim.shape[0],dtype=int))
+            neighbors_along_dim_repeated = np.kron(np.ones(neighbors.shape[0],dtype=int),neighbors_along_dim)
+            neighbors = previous_neighbors + neighbors_along_dim_repeated
+            # neighbors = np.append(neighbors,neighbors*np.prod(gs[:dd2+1]))
+        all_grid_vertices = np.arange(np.prod(gs_dd))
+        print(all_grid_vertices)
+        I = np.tile(all_grid_vertices,(neighbors.shape[0],1)).T
+        J = I + np.tile(neighbors,(all_grid_vertices.shape[0],1))
+        valid_indices = (J>=0)*(J<np.prod(gs_dd))
+        J = J[valid_indices]
+        I = I[valid_indices]
+        k1 = matrix_from_function(kernel_fun, grid_vertices, grid_vertices,sparsity_pattern=[I,J])
+        
+        # Could debug that we are building the proper matrix with this:
+        # k1_debug = matrix_from_function(kernel_fun, grid_vertices, grid_vertices)
+        # print(np.linalg.norm(k1.toarray()-k1_debug.toarray()))
+
+
         # Compute k2, the kernel matrix between the points in P and the points in the regular grid
-        k1 = matrix_from_function(kernel_fun, grid_vertices, grid_vertices)
+        if verbose:
+            print("Time to compute k1: ", time.time() - t00)
+            t00 = time.time()
         k2 = matrix_from_function(kernel_fun, P, grid_vertices)
+        if verbose:
+            print("Time to compute k2: ", time.time() - t00)
         # This is what we'd get if we did a true GP:
         # K3 = matrix_from_function(kernel_fun, P, P)
         # In reality, we approximate K3 with a lumped covariance matrix:
-        
-        K3 = diags(np.squeeze(sampling_density)) + sigma_n*sigma_n*eye(P.shape[0])
+        # K3 = diags(np.squeeze(sampling_density)) + sigma_n*sigma_n*eye(P.shape[0])
+        # But we really only need its inverse:
         K3_inv = diags(1/(np.squeeze(sampling_density) + sigma_n*sigma_n))
         
 
@@ -55,32 +117,55 @@ def poisson_surface_reconstruction(P,N,gs=None,h=None,corner=None,screened=False
         # print(K3_inv.shape)
         # print(N[:,dd][:,None].shape)
         means.append(k2.T @ K3_inv @ N[:,dd][:,None])
-        covs.append(k1 - k2.T @ K3_inv @ k2)
+        if stochastic:
+            covs.append(k1 - k2.T @ K3_inv @ k2)
 
+    
     # Concatenate the mean and covariance matrices
     vector_mean = np.concatenate(means)
-    vector_cov = sigma*sigma*block_diag(covs)
+    if stochastic:
+        vector_cov = sigma*sigma*block_diag(covs)
 
+    if verbose:
+        print("Total step 1 time: ", time.time() - t0)
     # Step 2: Solve Poisson equation on the regular grid
     
+
+    if verbose:
+        print("Step 2: Solve Poisson equation on the regular grid")
+        t1 = time.time()
+    if verbose:
+        t10 = time.time()
     G = fd_grad(gs=gs,h=h)
     mean_divergence = G.T @ vector_mean
     L = G.T @ G
-    mean_scalar, info = bicg(L + 0.0001*eye(L.shape[0]),mean_divergence,atol=1e-10)
+    mean_scalar, info = bicg(L + eps*eye(L.shape[0]),mean_divergence,atol=1e-10)
     assert(info==0)
     # Shift values of mean
     W = fd_interpolate(P,gs=gs,h=h,corner=corner)
     shift = np.sum((W @ mean_scalar) ) / P.shape[0]
     mean_scalar = mean_scalar - shift
+    if verbose:
+        print("Time to compute mean PDE solution: ", time.time() - t10)
+        t10 = time.time()
 
-
-    cov_divergence = G.T @ vector_cov @ G
-    lu = splu(L+0.0001*eye(L.shape[0]))
-    D = lu.solve(cov_divergence.toarray())
-    cov_scalar = lu.solve(D.transpose())
-    #cov = np.diag(spsolve(L+0.0001*eye(L.shape[0]),csc_matrix(D.transpose())).toarray())
-    var_scalar = np.diag(cov_scalar)
-    # Shift values of covariance
-    var_scalar = var_scalar - np.min(var_scalar) + sigma_n*sigma_n
-    
-    return mean_scalar, var_scalar
+    if stochastic:
+        cov_divergence = G.T @ vector_cov @ G
+        lu = splu(L+eps*eye(L.shape[0]))
+        D = lu.solve(cov_divergence.toarray())
+        cov_scalar = lu.solve(D.transpose())
+        #cov = np.diag(spsolve(L+0.0001*eye(L.shape[0]),csc_matrix(D.transpose())).toarray())
+        var_scalar = np.diag(cov_scalar)
+        # Shift values of covariance
+        var_scalar = var_scalar - np.min(var_scalar) + sigma_n*sigma_n
+        if verbose:
+            print("Time to compute covariance PDE solution: ", time.time() - t10)       
+        if verbose:
+            print("Total step 2 time: ", time.time() - t1)
+            print("Total time: ", time.time() - t0)
+        return mean_scalar, var_scalar
+    else:
+        if verbose:
+            print("Total step 2 time: ", time.time() - t1)
+            print("Total time: ", time.time() - t0)
+        return mean_scalar
--- test/test_poisson_surface_reconstruction.py
+++ test/test_poisson_surface_reconstruction.py
@@ -9,21 +9,28 @@ class TestPoissonSurfaceReconstruction(unittest.TestCase):
         np.random.seed(0)
         # First test: "uniform" sampling density
         # Sample points on a circle
-        th = 2*np.pi*np.random.rand(10,1)
+        th = 2*np.pi*np.random.rand(20,1)
         P = np.concatenate((np.cos(th) + 0.1,np.sin(th) + 0.2),axis=1)
         # Normals are the same as positions on a circle
         N = np.concatenate((np.cos(th),np.sin(th)),axis=1)
 
-        corner = np.array([-1.5,-1.5])
-        gs = np.array([30, 30])
-        h = np.array([0.1,0.1])
+        # corner = np.array([-1.5,-1.5])
+        gs = np.array([60, 60])
+        # h = np.array([0.05,0.05])
 
-        scalar_mean, scalar_var = gpytoolbox.poisson_surface_reconstruction(P,N,gs=gs,h=h,corner=corner)
-        # plt.imshow(scalar_var.reshape(gs,order='F'))
-        # plt.colorbar()
-        # plt.imshow(scalar_mean.reshape(gs,order='F'))
-        # plt.colorbar()
-        # plt.show()
+        scalar_mean, scalar_var = gpytoolbox.poisson_surface_reconstruction(P,N,gs=gs)
+
+        # Plot mean and variance side by side with colormap
+        fig, ax = plt.subplots(1,2)
+        ax[0].imshow(scalar_mean.reshape(gs,order='F'))
+        ax[0].set_title('Mean')
+        # Add colorbar
+        fig.colorbar(ax[0].imshow(scalar_mean.reshape(gs,order='F')), ax=ax[0])
+        ax[1].imshow(scalar_var.reshape(gs,order='F'))
+        ax[1].set_title('Variance')
+        # Add colorbar
+        fig.colorbar(ax[1].imshow(scalar_var.reshape(gs,order='F')), ax=ax[1])
+        plt.show()
         
 
 if __name__ == '__main__':
