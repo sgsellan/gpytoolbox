d15a5325482e84be7a036d73f3bed0d869bdc492
sgsellan <sgsellan@gmail.com>
2023-05-05 22:15:28 -0700

    init

---
+++ src/gpytoolbox/marching_squares.py
@@ -0,0 +1,115 @@
+import numpy as np
+from .remove_duplicate_vertices import remove_duplicate_vertices
+
+def marching_squares(S,GV,nx,ny):
+    """
+    Marching squares algorithm for extracting isocontours from a scalar field.
+    S: scalar field
+    nx,ny: number of grid points in x and y direction
+    """
+    S = np.reshape(S,(nx,ny),order='F')
+    # Create empty list for
+    verts = []
+    edge_list = [] # index of edge vertices
+    # Loop over all grid points
+    for i in range(nx-1):
+        for j in range(ny-1):
+            # Get the scalar values at the corners of the grid cell
+            a = S[i,j]
+            b = S[i+1,j]
+            c = S[i+1,j+1]
+            d = S[i,j+1]
+            # Get the contour index
+            k = 0
+            if a > 0:
+                k += 1
+            if b > 0:
+                k += 2
+            if c > 0:
+                k += 4
+            if d > 0:
+                k += 8
+            # Use symmetry
+            if k > 7:
+                k = 15 - k
+            
+            # Get the contour line segments
+            if k == 1:
+                # x = i
+                x = i - a/(b-a)
+                y = j
+                verts.append([x,y])
+                x = i
+                y = j - a/(d-a)
+                # y = j
+                verts.append([x,y])
+                edge_list.append([len(verts)-2,len(verts)-1])
+            elif k == 2:
+                x = i - a/(b-a)
+                # x = i
+                y = j
+                verts.append([x,y])
+                x = i + 1
+                y = j - b/(c-b)
+                verts.append([x,y])
+                edge_list.append([len(verts)-2,len(verts)-1])
+            elif k == 3:
+                x = i
+                y = j - a/(d-a)
+                verts.append([x,y])
+                x = i + 1
+                y = j - b/(c-b)
+                verts.append([x,y])
+                edge_list.append([len(verts)-2,len(verts)-1])
+            elif k == 4:
+                x = i + 1
+                y = j- b/(c-b)
+                verts.append([x,y])
+                x = i - d/(c-d)
+                y = j + 1
+                verts.append([x,y])
+                edge_list.append([len(verts)-2,len(verts)-1])
+            elif k == 5:
+                x = i - a/(b-a)
+                y = j
+                verts.append([x,y])
+                x = i
+                y = j - a/(d-a)
+                # y = j
+                verts.append([x,y])
+                x = i + 1
+                y = j- b/(c-b)
+                verts.append([x,y])
+                x = i - d/(c-d)
+                y = j + 1
+                verts.append([x,y])
+                edge_list.append([len(verts)-4,len(verts)-3])
+                edge_list.append([len(verts)-2,len(verts)-1])
+            elif k == 6:
+                x = i - a/(b-a)
+                y = j
+                verts.append([x,y])
+                x = i - d/(c-d)
+                y = j + 1
+                verts.append([x,y])
+                edge_list.append([len(verts)-2,len(verts)-1])
+            elif k == 7:
+                x = i - d/(c-d)
+                y = j + 1
+                verts.append([x,y])
+                x = i
+                y = j - a/(d-a)
+                verts.append([x,y])
+                edge_list.append([len(verts)-2,len(verts)-1])
+            else:
+                pass
+    # Convert list to numpy array
+    verts = np.array(verts)
+    verts, SVI, SVJ, edges = remove_duplicate_vertices(verts,faces=np.array(edge_list))
+
+    # Rescale to original grid
+    verts[:,0] = verts[:,0]/(nx-1)
+    verts[:,1] = verts[:,1]/(ny-1)
+    verts = verts*(GV.max(axis=0)-GV.min(axis=0)) + GV.min(axis=0)
+
+    return verts, edges
---
+++ test/test_marching_squares.py
@@ -0,0 +1,49 @@
+import numpy as np
+from .context import gpytoolbox
+from .context import unittest
+import matplotlib.pyplot as plt
+
+class TestMarchingSquares(unittest.TestCase):
+    # This isn't too complex, probably could use being expanded
+    def test_circle(self):
+        # Build a polyline; for example, a square
+        n = 200
+        gx, gy = np.meshgrid(np.linspace(-1.0, 1.0, n+1), np.linspace(-1.0, 1.0, n+1), indexing='ij')
+        GV = np.vstack((gx.flatten(), gy.flatten())).T
+        S = np.linalg.norm(GV, axis=1)-0.5
+        verts, edge_list = gpytoolbox.marching_squares(S, GV, n+1, n+1)
+        # All verts should have zero sdf
+        verts_S = (np.linalg.norm(verts, axis=1)-0.5)**2.0
+        # print(np.min(verts_S))
+        self.assertTrue(np.allclose(verts_S, 0.0))
+    def test_png(self):
+        # Build a polyline; for example, a square
+        n = 100
+        gx, gy = np.meshgrid(np.linspace(-1.0, 1.0, n+1), np.linspace(-1.0, 1.0, n+1))
+        GV = np.vstack((gx.flatten(), gy.flatten())).T
+        filename = "test/unit_tests_data/illustrator.png"
+        poly = gpytoolbox.png2poly(filename)[0]
+        poly = poly[::10,:]
+        poly = gpytoolbox.normalize_points(poly)
+        # plt.plot(poly[:,0],poly[:,1])
+        # plt.show()
+        E = gpytoolbox.edge_indices(poly.shape[0],closed=True)
+        S = gpytoolbox.signed_distance(GV, poly, E)[0]
+        # S = np.linalg.norm(GV, axis=1)-0.5
+        verts, edge_list = gpytoolbox.marching_squares(S, GV, n+1, n+1)
+        # # All verts should have zero sdf
+        verts_S = gpytoolbox.signed_distance(verts, poly, E)[0]**2.0
+        self.assertTrue(np.allclose(verts_S, 0.0,atol=1e-4))
+        
+
+        # for i in range(edge_list.shape[0]):
+        #     plt.plot([verts[edge_list[i,0],0],verts[edge_list[i,1],0]],
+        #              [verts[edge_list[i,0],1],verts[edge_list[i,1],1]],
+        #              'k-')
+            
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
