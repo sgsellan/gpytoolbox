a3a2474a197f525c6f8cf9adf035fcd87fc3ceb5
sgsellan <sgsellan@gmail.com>
2023-05-08 19:44:08 -0400

    use winding number to sign

--- src/gpytoolbox/signed_distance.py
+++ src/gpytoolbox/signed_distance.py
@@ -58,25 +58,29 @@ def signed_distance(Q,V,F=None,use_cpp=True):
         # Get the sign in linear complexity using the trick in https://www.shadertoy.com/view/wdBXRW , would be nice to have a log n BVH implementation of this sign for 2D
         n = F.shape[0]
         s = 1.0
-        for i in range(0,n):
-            # if i==0:
-            #     j = n-1
-            # else:
-            #     j = i-1
-            # e = V[j,:] - V[i,:]
-            vj = V[F[i,0],:]
-            vi = V[F[i,1],:]
-            e = vj - vi
-            # Skip if the edge is zero length
-            if np.sum(e*e)==0:
-                continue
-            w = (Q - np.tile(V[i,:],(Q.shape[0],1)))  
-            cond = np.array([ Q[:,1]>=vi[1], Q[:,1]<=vj[1], (e[0]*w[:,1])>(e[1]*w[:,0]) ])
-            all_true = cond.all(axis=0)
-            all_false = (~cond).all(axis=0)
-            change_sign = all_true + all_false
-            s = ((-np.ones((Q.shape[0])))**(change_sign))*s
-        signed_distance = np.sqrt(sqrD)*s
+        # for i in range(0,n):
+        #     # if i==0:
+        #     #     j = n-1
+        #     # else:
+        #     #     j = i-1
+        #     # e = V[j,:] - V[i,:]
+        #     vj = V[F[i,0],:]
+        #     vi = V[F[i,1],:]
+        #     e = vj - vi
+        #     # Skip if the edge is zero length
+        #     if np.sum(e*e)==0:
+        #         continue
+        #     w = (Q - np.tile(V[i,:],(Q.shape[0],1)))  
+        #     cond = np.array([ Q[:,1]>=vi[1], Q[:,1]<=vj[1], (e[0]*w[:,1])>(e[1]*w[:,0]) ])
+        #     all_true = cond.all(axis=0)
+        #     all_false = (~cond).all(axis=0)
+        #     change_sign = all_true + all_false
+        #     s = ((-np.ones((Q.shape[0])))**(change_sign))*s
+        # signed_distance = np.sqrt(sqrD)*s
+        W = winding_number(V,F,Q)
+        W = np.sign(-2*W+1)
+        dist = np.sqrt(sqrD)
+        signed_distance = W*dist
         # print(s)
 
     else:
@@ -92,3 +96,25 @@ def signed_distance(Q,V,F=None,use_cpp=True):
     return signed_distance, I, lmbd
 
     
+
+### TEMPORARY
+def solid_angle(V, F, O):
+     # Assuming V and F are already defined as numpy arrays
+     VS = V[F[:, 0], :]
+     VD = V[F[:, 1], :]
+
+     # 2D vectors from O to VS and VD
+     O2VS = np.expand_dims(O[:, :2], axis=1) - np.expand_dims(VS[:, :2], axis=0)
+     O2VD = np.expand_dims(O[:, :2], axis=1) - np.expand_dims(VD[:, :2], axis=0)
+
+     # Commented out normalization as per original Matlab script
+     # O2VS = O2VS / np.linalg.norm(O2VS, axis=2, keepdims=True)
+     # O2VD = O2VD / np.linalg.norm(O2VD, axis=2, keepdims=True)
+
+     S = -np.arctan2(O2VD[:, :, 0] * O2VS[:, :, 1] - O2VD[:, :, 1] * O2VS[:, :, 0], O2VD[:, :, 0] * O2VS[:, :, 0] + O2VD[:, :, 1] * O2VS[:, :, 1])
+     return S
+
+def winding_number(V, F, O):
+     S = solid_angle(V, F, O)
+     W = np.sum(S, axis=1) / (2 * np.pi)
+     return W
