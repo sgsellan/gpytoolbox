073863d2c9148310bab6a22a76f6307707936ad7
Silvia Sellán <sgsellan@gmail.com>
2023-07-02 17:25:13 +0200

    Fixing-windows-build (#77)
    fixes #72

--- cmake/libigl.cmake
+++ cmake/libigl.cmake
@@ -6,6 +6,6 @@ include(FetchContent)
 FetchContent_Declare(
     libigl
     GIT_REPOSITORY https://github.com/libigl/libigl.git
-    GIT_TAG 0c77359c89f3cd7ae5cdee09e7175953fe41a3c8
+    GIT_TAG 282388c68a0e1de325185f7866c296b3ed10a7f5
 )
 FetchContent_MakeAvailable(libigl)
