975173b63a4b97542b29c239db4cf0594c3189c5
sgsellan <sgsellan@gmail.com>
2023-07-04 00:16:15 +0200

    normalization is back

--- test/test_marching_cubes.py
+++ test/test_marching_cubes.py
@@ -2,6 +2,7 @@ from .context import gpytoolbox
 from .context import numpy as np
 from .context import unittest
 # import polyscope as ps
+# import igl
 
 class TestMarchingCubes(unittest.TestCase):
     def test_meshes(self):
@@ -9,14 +10,8 @@ class TestMarchingCubes(unittest.TestCase):
         for mesh in meshes:
             # num_points = 1000
             V,F = gpytoolbox.read_mesh("test/unit_tests_data/" +mesh)
-            # print(F[0,:])
-            # print(V[0,:])
-            # ps.init()
-            # ps.register_surface_mesh("input",V,F)
-            # N = gpytoolbox.per_vertex_normals(V,F)
-            # normals = ps.register_point_cloud("normals",V)
-            # normals.add_vector_quantity("normals",N)
-            # ps.show()
+            V = gpytoolbox.normalize_points(V,center=np.array([0.5,0.5,0.5]))
+            # V, _, N, F, _, _ = igl.read_obj("test/unit_tests_data/" +mesh)
             # # ps.register_point_cloud("input",gpytoolbox.barycenters(V,F))
             # ps.show()
             # Normalize mesh
@@ -31,6 +26,7 @@ class TestMarchingCubes(unittest.TestCase):
             U,G = gpytoolbox.marching_cubes(s1,GV,gs,gs,gs,0.0)
             # Get winding number now using the isosurface
             s2 = gpytoolbox.signed_distance(GV,U,G)[0]
+
             self.assertTrue(np.isclose(s1,s2,atol=1e-2).all())
             # Now the claim is that U,G and V,F should be "similar"
             dists = gpytoolbox.squared_distance(U,V,F=F,use_cpp=True)[0]
