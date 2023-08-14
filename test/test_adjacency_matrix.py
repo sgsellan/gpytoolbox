48a6775825b94e7325bd4f38547e4a867ed0cd68
jpvt <joaoteixeira@eng.ci.ufpb.br>
2023-08-14 17:38:06 -0300

    Add adjacency matrix simple test

--- test/test_adjacency_matrix.py
+++ test/test_adjacency_matrix.py
@@ -1,11 +1,28 @@
 from .context import gpytoolbox as gpy
+from scipy.sparse import csc_matrix
 from .context import numpy as np
 from .context import unittest
 
 class TestAdjacencyMatrix(unittest.TestCase):
-    def test_false(self):
-        # To-do
-        self.assertTrue(False)
+    def test_basic(self):
+        F = np.array([
+            [0, 1, 2],
+            [2, 3, 4],
+            [1, 2, 5]
+        ])
+
+        expected = csc_matrix([
+            [0, 1, 1, 0, 0, 0],
+            [1, 0, 1, 0, 0, 1],
+            [1, 1, 0, 1, 1, 1],
+            [0, 0, 1, 0, 1, 0],
+            [0, 0, 1, 1, 0, 0],
+            [0, 1, 1, 0, 0, 0]
+        ])
+
+        result = gpy.adjacency_matrix(F)
+
+        self.assertTrue((result.toarray() == expected.toarray()).all())
 
 
 if __name__ == '__main__':
