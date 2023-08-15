7c1cd0563a09cb5d9f004e7775231e5044f5327f
jpvt <joaoteixeira@eng.ci.ufpb.br>
2023-08-15 20:47:01 -0300

    add mean curvature

---
+++ src/gpytoolbox/discrete_mean_curvature.py
@@ -0,0 +1,38 @@
+import numpy as np
+from .adjacency_dihedral_angle_matrix import adjacency_dihedral_angle_matrix
+
+def edge_lengths(V, F):
+    e1 = np.linalg.norm(V[F[:, 1], :] - V[F[:, 0], :], axis=1)
+    e2 = np.linalg.norm(V[F[:, 2], :] - V[F[:, 1], :], axis=1)
+    e3 = np.linalg.norm(V[F[:, 0], :] - V[F[:, 2], :], axis=1)
+    return e1, e2, e3
+
+def discrete_mean_curvature(V, F):
+    # Calculate dihedral angles
+    A, _ = adjacency_dihedral_angle_matrix(V, F)
+    A = A.toarray()
+    
+    # Calculate edge lengths for each face
+    L1, L2, L3 = edge_lengths(V, F)
+    
+    # Integrated mean curvature for each vertex
+    H = np.zeros(V.shape[0])
+    for i, face in enumerate(F):
+        for j, v in enumerate(face):
+            # For each vertex, we need to find the corresponding edges and their lengths
+            if j == 0:
+                L = L1[i]
+                neighbors = [face[1], face[2]]
+            elif j == 1:
+                L = L2[i]
+                neighbors = [face[0], face[2]]
+            else:
+                L = L3[i]
+                neighbors = [face[0], face[1]]
+
+            # Add contribution of each edge to the integrated mean curvature
+            # Keenan's formula from DDG Slide 21 Lecture16 DiscreteCurvature II
+            for neighbor in neighbors:
+                H[v] += 0.5 * L * A[i, neighbor]
+    
+    return H
