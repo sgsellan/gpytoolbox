477e8460e59d45079bb5e03429ab72243e6ad888
jpvt <joaoteixeira@eng.ci.ufpb.br>
2023-08-15 21:04:57 -0300

    fix mean curv test name

--- test/test_discrete_mean_curvature.py
+++ test/test_discrete_mean_curvature.py
@@ -2,8 +2,8 @@ from .context import gpytoolbox as gpy
 from .context import numpy as np
 from .context import unittest
 
-class TestDiscreteGaussianCurvature(unittest.TestCase):
-    def test_bunny(self):
+class TestDiscreteMeanCurvature(unittest.TestCase):
+    def test_bunny_mock(self):
         V,F = gpy.read_mesh("test/unit_tests_data/bunny_oded.obj")
         
         result_h = gpy.discrete_mean_curvature(V,F)
