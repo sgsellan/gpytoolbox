687acfd90f3f4dfdce2a9a87a7d3fec570e7f7e0
Oded Stein <oded.stein.graphics@gmail.com>
2023-10-18 20:47:16 -0700

    a few new sdf_flow unit tests

--- test/test_sdf_flow.py
+++ test/test_sdf_flow.py
@@ -40,5 +40,22 @@ class TestRFTS(unittest.TestCase):
             h = gpy.approximate_hausdorff_distance(U, G.astype(np.int32), v, f.astype(np.int32), use_cpp=True)
             self.assertTrue(h < 2e-3)
 
+    def test_simple_is_sdf_violated(self):
+        meshes = ["cube.obj", "hemisphere.obj", "cone.obj"]
+        for mesh in meshes:
+            n = 30
+            v, f = gpy.read_mesh("test/unit_tests_data/" + mesh)
+            v = gpy.normalize_points(v)
+
+            # Create and abstract SDF function that is the only connection to the shape
+            sdf = lambda x: gpy.signed_distance(x, v, f)[0]
+            gx, gy, gz = np.meshgrid(np.linspace(-1.0, 1.0, n+1), np.linspace(-1.0, 1.0, n+1), np.linspace(-1.0, 1.0, n+1))
+            GV = np.vstack((gx.flatten(), gy.flatten(), gz.flatten())).T
+            V0, F0 = gpy.icosphere(2)
+            U,G = gpy.sdf_flow(GV, sdf, V0, F0, verbose=False, visualize=False, min_h = 0.5/n)
+
+            sdf_rec = lambda x: gpy.signed_distance(x, U, G)[0]
+            self.assertTrue(np.max(np.abs(sdf(GV)-sdf_rec(GV))) < 0.02)
+
 if __name__ == '__main__':
     unittest.main()
