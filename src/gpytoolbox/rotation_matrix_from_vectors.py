68af59d5f7508f0b4fed201e8b60ae612862466f
Silvia Sellán <sgsellan@gmail.com>
2023-12-04 18:15:56 -0500

    2d, plus better test and reflection check

--- src/gpytoolbox/rotation_matrix_from_vectors.py
+++ src/gpytoolbox/rotation_matrix_from_vectors.py
@@ -5,12 +5,12 @@ def rotation_matrix_from_vectors(vec1, vec2):
 
     Parameters
     ----------
-    vec1: (3,) numpy array
-    vec2: (3,) numpy array
+    vec1: (2,) or (3,) numpy array
+    vec2: (2,) or (3,) numpy array
 
     Returns
     -------
-    R: (3,3) numpy array such that R.dot(vec1) ~= vec2
+    R: (2,2) or (3,3) numpy array such that R.dot(vec1) ~= vec2
 
     Notes
     -----
@@ -34,13 +34,31 @@ def rotation_matrix_from_vectors(vec1, vec2):
     ```
     """
 
-    a, b = (vec1 / np.linalg.norm(vec1)).reshape(3), (vec2 / np.linalg.norm(vec2)).reshape(3)
-    v = np.cross(a, b)
-    if any(v): #if not all zeros then 
-        c = np.dot(a, b)
-        s = np.linalg.norm(v)
-        kmat = np.array([[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]])
-        return np.eye(3) + kmat + kmat.dot(kmat) * ((1 - c) / (s ** 2))
+    # normalized
+    a, b = (vec1 / np.linalg.norm(vec1)), (vec2 / np.linalg.norm(vec2))
+    
+    dim = len(a)
+    assert dim == len(b)
 
+    # this is dimension agnostic even if it doesn't seem like it
+    v = np.cross(a, b).flatten()
+    if any(v): #if not all zeros then 
+        if dim==2:
+            theta = np.arccos(np.dot(a, b))
+            # with correct sign
+            if np.cross(a, b) < 0:
+                theta = -theta
+            R = np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]])
+            return R
+        else:
+            assert dim == 3
+            c = np.dot(a, b)
+            s = np.linalg.norm(v)
+            kmat = np.array([[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]])
+            return np.eye(3) + kmat + kmat.dot(kmat) * ((1 - c) / (s ** 2))
     else:
-        return np.eye(3) #cross of all zeros only occurs on identical directions
+        # is it a reflection?
+        R = np.eye(dim)
+        if np.dot(a, b) < 0:
+            R = -R
+        return R
--- test/test_rotation_matrix_from_vectors.py
+++ test/test_rotation_matrix_from_vectors.py
@@ -3,7 +3,7 @@ from .context import numpy as np
 from .context import unittest
 
 class TestRotationMatrixFromVectors(unittest.TestCase):
-    def test_randomly_generated(self):
+    def test_randomly_generated_3d(self):
         np.random.seed(0)
         for i in range(10000):
             u = np.random.randn(3)
@@ -18,7 +18,34 @@ class TestRotationMatrixFromVectors(unittest.TestCase):
             u = u/np.linalg.norm(u)
             R = gpy.rotation_matrix_from_vectors(u, v)
             self.assertTrue(np.allclose(R.dot(u), v, atol=1e-12))
+            # test special case of reflection
+            v = -u
+            R = gpy.rotation_matrix_from_vectors(u, v)
+            self.assertTrue(np.allclose(R.dot(u), v, atol=1e-12))
 
+
+    def test_randomly_generated_2d(self):
+        np.random.seed(0)
+        for i in range(10000):
+            u = np.random.randn(2)
+            # normalize
+            v = np.random.randn(2)
+            R = gpy.rotation_matrix_from_vectors(u, v)
+            # check that they are aligned in 2D
+            are_aligned = np.cross(v, R.dot(u))
+            print(R.dot(u))
+            print(v)
+            print(are_aligned)
+            self.assertTrue(np.allclose(are_aligned, np.zeros(1), atol=1e-12))
+            # normalize
+            v = v/np.linalg.norm(v)
+            u = u/np.linalg.norm(u)
+            R = gpy.rotation_matrix_from_vectors(u, v)
+            self.assertTrue(np.allclose(R.dot(u), v, atol=1e-12))
+            # test special case of reflection
+            v = -u
+            R = gpy.rotation_matrix_from_vectors(u, v)
+            self.assertTrue(np.allclose(R.dot(u), v, atol=1e-12))
 
 if __name__ == '__main__':
     unittest.main()
