4ddf997a9f46ec27e27839e77d3425e348754b32
Silvia Sellán <sgsellan@gmail.com>
2024-06-01 20:27:00 +0200

    trying old webgpu?

--- cmake/glfw3webgpu.cmake
+++ cmake/glfw3webgpu.cmake
@@ -6,6 +6,6 @@ include(FetchContent)
 FetchContent_Declare(
     glfw3webgpu
     GIT_REPOSITORY https://github.com/eliemichel/glfw3webgpu.git
-    GIT_TAG e4cd9131c7472b000641a104116c2fecf13d55a7
+    GIT_TAG 22e54ff192d9af967484ccf58570fe06fde1e969
 )
 FetchContent_MakeAvailable(glfw3webgpu)
