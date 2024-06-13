eeb29c37aca430fc4a2657f14939b6f22e293163
Silvia Sellán <sgsellan@gmail.com>
2024-06-13 07:58:09 -0400

    debug prints

--- test/test_lazy_cage.py
+++ test/test_lazy_cage.py
@@ -8,9 +8,13 @@ class TestLazyCage(unittest.TestCase):
         # Generate many examples
         for m in np.linspace(20,2000,10,dtype=int):
             # print(m)
+            print("Calling lazy_cage with m = ", m)
             u,g = gpytoolbox.copyleft.lazy_cage(v,f,num_faces=m,max_iter=10)
+            print("Finished lazy_cage with m = ", m)
             # u,g fully contains v,f, i.e. they don't intersect
+            print("Checking if u,g fully contains v,f")
             b, _ = gpytoolbox.copyleft.do_meshes_intersect(v,f,u,g)
+            print("Finished checking if u,g fully contains v,f")
             self.assertFalse(b)
     def test_armadillo(self):
         v,f = gpytoolbox.read_mesh("test/unit_tests_data/armadillo.obj")
@@ -18,9 +22,13 @@ class TestLazyCage(unittest.TestCase):
         # Generate many examples
         for m in np.linspace(1000,2000,5,dtype=int):
             # print(m)
+            print("Calling lazy_cage with m = ", m)
             u,g = gpytoolbox.copyleft.lazy_cage(v,f,num_faces=m,max_iter=10)
+            print("Finished lazy_cage with m = ", m)
             # u,g fully contains v,f, i.e. they don't intersect
+            print("Checking if u,g fully contains v,f")
             b, _ = gpytoolbox.copyleft.do_meshes_intersect(v,f,u,g)
+            print("Finished checking if u,g fully contains v,f")
             self.assertFalse(b)
             
 if __name__ == '__main__':
