27338e5d7bb512a76b8c9f77b60d594a577cdd3a
Silvia Sellán <sgsellan@gmail.com>
2025-03-17 17:24:36 -0400

    libigl bump

--- cmake/libigl.cmake
+++ cmake/libigl.cmake
@@ -6,6 +6,6 @@ include(FetchContent)
 FetchContent_Declare(
     libigl
     GIT_REPOSITORY https://github.com/libigl/libigl.git
-    GIT_TAG 282388c68a0e1de325185f7866c296b3ed10a7f5
+    GIT_TAG ba69acc509632c56c738f03440841cc37da50497
 )
 FetchContent_MakeAvailable(libigl)
