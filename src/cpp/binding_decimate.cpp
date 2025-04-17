c111a4a8d93a701b17d9d0b5a33b7f962eee7502
Silvia Sellán <sgsellan@gmail.com>
2025-04-17 10:15:48 -0400

    decimate debugging leftover

--- src/cpp/binding_decimate.cpp
+++ src/cpp/binding_decimate.cpp
@@ -30,16 +30,16 @@ void binding_decimate(py::module& m) {
     //         igl::decimate_post_collapse_callback post_collapse;
     // igl::decimate_trivial_callbacks(pre_collapse,post_collapse);
             if(method==0) {
-                std::cout << "Decimating with method 0" << std::endl;
-                igl::writeOBJ("decimate_input.obj",V,F);
-                std::cout << "Wrote input to decimate_input.obj" << std::endl;
-                std::cout << "Number of faces: " << num_faces << std::endl;
+                // std::cout << "Decimating with method 0" << std::endl;
+                // igl::writeOBJ("decimate_input.obj",V,F);
+                // std::cout << "Wrote input to decimate_input.obj" << std::endl;
+                // std::cout << "Number of faces: " << num_faces << std::endl;
                 igl::decimate(V,F,num_faces,
                     //This will be required when we bump the libigl version.
                     block_intersections,
                     SV,SF,I,J);
             } else if(method==1) {
-                std::cout << "Decimating with method 1" << std::endl;
+                // std::cout << "Decimating with method 1" << std::endl;
                 
                 igl::qslim(V,F,num_faces,
                     //This will be required when we bump the libigl version.
